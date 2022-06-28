6341591afb6267afc6c68f870a8c1dd5e028b59f
Em <ekramm@me.com>
2022-06-28 11:07:55 -0400

    commit tree e654bb09b58caf47908ed342c39e0fe6bcd041c6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+                  Student.find_by(params[:name])
+                else
+                  Student.all
+                end
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
